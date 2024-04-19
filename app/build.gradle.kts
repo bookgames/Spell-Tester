5ed483e5d652a81c3fb4d051e9d07739eda45680
KhaledHawwas <82279619+KhaledHawwas@users.noreply.github.com>
2024-04-19 14:55:28 +0200

    Update gradle files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
     kotlin("kapt")
 }
 
@@ -46,8 +45,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
     }
 }
 dependencies {
@@ -56,16 +55,17 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.chromium.net:cronet-embedded:119.6045.31")
 
 
     // Room and Architectural Components
-    val room_version="2.3.0"
-    implementation("androidx.room:room-runtime:$room_version")
+    val roomVersion="2.3.0"
+    implementation("androidx.room:room-runtime:$roomVersion")
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
     implementation ("androidx.lifecycle:lifecycle-extensions:2.1.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.1.0")
     implementation ("androidx.room:room-ktx:2.2.1")
-    kapt ("androidx.room:room-compiler:$room_version")
+    kapt ("androidx.room:room-compiler:$roomVersion")
 
     // Coroutines
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
@@ -73,20 +73,28 @@ dependencies {
 
     //stack bar
     implementation ("com.github.nisrulz:stackedhorizontalprogressbar:1.0.3")
+
     // for adding recyclerview
     implementation("androidx.recyclerview:recyclerview:1.2.0")
-
+    //Gson
+    implementation("com.google.code.gson:gson:2.8.8")
     // for adding cardview
     implementation("androidx.cardview:cardview:1.0.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.38.1")
     kapt("com.google.dagger:hilt-compiler:2.38.1")
+
     // firebase
     implementation("com.google.firebase:firebase-analytics")
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-firestore-ktx:24.10.2")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
 }
